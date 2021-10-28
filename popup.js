0d09d38c3f2dc25ab99c0861a635701bfcfea861
Phillip <phillipyoo.218@gmail.com>
2021-10-27 20:50:26 -0400

    first

---
+++ popup.js
@@ -0,0 +1,14 @@
+{
+  "manifest_version": 3,
+  "name": "FILL",
+  "description": "FILL",
+  "version": "1.0",
+  "action": {
+      "default_icon": "OUR PIC",
+      "default_popup": "popup.html"
+  },
+
+  "permissions": [
+     
+  ]
+}
